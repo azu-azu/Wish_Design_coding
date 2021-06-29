bc41d35017d24345a15d4f4386ec7f776d19f03c
azu-azu <azu-azu@users.noreply.github.com>
2021-06-29 21:46:22 +0900

    scrollify追加

--- js/script.js
+++ js/script.js
@@ -135,6 +135,19 @@ fitty('[js-fitty]', {
 
 
 
+// スクロール設定
+var option = {
+  section : '.js-scroll--area', // 対象を指定
+  easing: "swing", // イージングをしてい(jQueryのanimation)
+  scrollSpeed: 600, // スクロール時の速度
+  scrollbars: true, // スクロールバーを表示するか
+};
+
+$(function() {
+  $.scrollify(option); // scrollifyの実行
+});
+
+
 
 
 
